Fixed a bug in the prediction training. Split encoding and training. Adding some methods to change averaging.

dataloader.py
@@ -16,10 +16,10 @@ from load_data import getMatches, normalize
 
 
 class EncoderDataset(Dataset):
-    def __init__(self, datapath: str = './data', split: float = 1., is_val: bool = False, seed: int = 7):
+    def __init__(self, datapath: str = './data', split: float = 1., method: str = 'Averages', is_val: bool = False, seed: int = 7):
         # Load in data
         self.datapath = datapath
-        av_df = pd.read_csv(f'{self.datapath}/MAverages.csv').set_index(['season', 'tid'])
+        av_df = pd.read_csv(Path(f'{self.datapath}/{method}.csv')).set_index(['season', 'tid'])
 
         Xt, Xs, yt, ys = train_test_split(av_df, av_df, test_size=split, random_state=seed)
         self.data = torch.tensor(Xs.values, dtype=torch.float32) if is_val else torch.tensor(Xt.values, dtype=torch.float32)
@@ -44,6 +44,7 @@ class EncoderDataModule(LightningDataModule):
             device: str = 'cpu',
             datapath: str = './data',
             split: float = .7,
+            averaging_method: str = 'Averages',
             **kwargs,
     ):
         super().__init__()
@@ -58,10 +59,11 @@ class EncoderDataModule(LightningDataModule):
         self.device = device
         self.datapath = datapath
         self.split = split
+        self.method = averaging_method
 
     def setup(self, stage: Optional[str] = None) -> None:
-        self.train_dataset = EncoderDataset(self.datapath, self.split)
-        self.val_dataset = EncoderDataset(self.datapath, self.split, is_val=True)
+        self.train_dataset = EncoderDataset(self.datapath, self.split, self.method)
+        self.val_dataset = EncoderDataset(self.datapath, self.split, self.method, is_val=True)
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(

model.py
@@ -1,8 +1,6 @@
 from typing import Any
-
 import numpy as np
 from bayesian_torch.models.dnn_to_bnn import dnn_to_bnn
-from torch.distributions import MultivariateNormal
 import torch
 from pytorch_lightning import LightningModule
 from torch import nn, optim, Tensor
@@ -25,6 +23,8 @@ class Encoder(LightningModule):
 
         self.encoder = nn.Sequential(
             nn.Linear(self.init_size, self.latent_size),
+            nn.SiLU(),
+            nn.Linear(self.latent_size, self.latent_size),
             nn.SiLU(),
             nn.Linear(self.latent_size, self.latent_size),
         )
@@ -101,7 +101,7 @@ class Encoder(LightningModule):
 
 class Predictor(LightningModule):
 
-    def __init__(self, init_size: int = 62, latent_size: int = 62, lr: float = 1e-5, weight_decay: float = 0.0,
+    def __init__(self, init_size: int = 70, latent_size: int = 100, lr: float = 1e-5, weight_decay: float = 0.0,
                  encoded_sz: int = 10, sigma: float = 10., scheduler_gamma: float = .7, betas: tuple[float, float] = (.9, .99), *args: Any, **kwargs: Any):
         super().__init__()
         self.init_size = init_size

prep_data.py
@@ -1,12 +1,21 @@
 from pathlib import Path
-
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 import pandas as pd
 import yaml
+from load_data import addSeasonalStatsToFrame, normalize
 
-from load_data import getMatches
+
+def gauss_weight(df, col, sigma=None):
+    sigma = df[f't_{col}'].std() if sigma is None else sigma
+    df_weight = np.exp(-(df[f't_{col}'] - df[f'o_{col}']) ** 2 / (2 * sigma ** 2))
+    return df.mul(df_weight, axis=0).groupby(['season', 'tid']).sum().mul(
+        1 / df_weight.groupby(['season', 'tid']).sum(), axis=0)
+
+
+def col_weight(df, col):
+    df_weight = df[col]
+    return df.mul(df_weight, axis=0).groupby(['season', 'tid']).sum().mul(
+        1 / df_weight.groupby(['season', 'tid']).sum(), axis=0)
 
 if __name__ == '__main__':
 
@@ -17,16 +26,38 @@ if __name__ == '__main__':
             print(exc)
 
     datapath = config['dataloader']['datapath']
-    data = pd.read_csv(Path(f'{datapath}\\MGameDataBasic.csv')).set_index(['gid', 'season', 'tid', 'oid'])
-    feats = pd.read_csv(Path(f'{datapath}\\MEncodedData.csv')).set_index(['season', 'tid'])
+    adf = pd.read_csv(Path(f'{datapath}\\MGameDataAdv.csv')).set_index(['gid', 'season', 'tid', 'oid'])
+    adf = adf.loc(axis=0)[:, 2004:]
 
-    t0, t1 = getMatches(data, feats)
-    results = data['t_score'] - data['o_score'] > 0
+    # Averaging using various methods over whole dataset, not averages
+    # Save these as different datasets for training
+    for method in ['Simple', 'Gauss', 'Elo', 'Rank']:
+        if method == 'Simple':
+            avdf = adf.groupby(['season', 'tid']).mean()
+        elif method == 'Gauss':
+            avdf = gauss_weight(adf, 'elo')
+        elif method == 'Elo':
+            avdf = col_weight(adf, 'o_elo')
+        elif method == 'Rank':
+            avdf = gauss_weight(adf, 'rank')
+        avdf = addSeasonalStatsToFrame(adf, avdf, True)
+        avdf = avdf.drop(columns=['numot'])
+        avdf_norm = normalize(avdf, to_season=True)
+        avdf_norm.to_csv(Path(f'{config["load_data"]["save_path"]}/M{method}Averages.csv'))
 
-    Xs0, Xt0, Xs1, Xt1, ys, yt = train_test_split(t0, t1, results, test_size=.3)
-
-    rfc = RandomForestClassifier()
-    rfc.fit(Xs0 - Xs1, ys.values.ravel())
-
-    test_res = rfc.predict(Xt0 - Xt1)
-    res_acc = sum([x == y for x, y in zip(yt.values.flatten(), test_res)]) / Xt0.shape[0]
+    # Averaging using various methods over normalized data
+    # Is this significant? Dunno.
+    nadf = normalize(adf, to_season=True)
+    for method in ['Simple', 'Gauss', 'Elo', 'Rank']:
+        if method == 'Simple':
+            avdf = nadf.groupby(['season', 'tid']).mean()
+        elif method == 'Gauss':
+            avdf = gauss_weight(nadf, 'elo')
+        elif method == 'Elo':
+            avdf = col_weight(nadf, 'o_elo')
+        elif method == 'Rank':
+            avdf = gauss_weight(nadf, 'rank')
+        avdf = addSeasonalStatsToFrame(adf, avdf, True)
+        avdf = avdf.drop(columns=['numot'])
+        avdf_norm = normalize(avdf, to_season=True)
+        avdf_norm.to_csv(Path(f'{config["load_data"]["save_path"]}/MNormalized{method}Averages.csv'))

train.py
@@ -22,59 +22,7 @@ if __name__ == '__main__':
             config = yaml.safe_load(file)
         except yaml.YAMLError as exc:
             print(exc)
-    # seed_everything(43, workers=True)
-
-    if config['encoder']['run_train']:
-        # First, run the encoding to try and reduce the dimension of the data
-        enc_data = EncoderDataModule(**config['dataloader'])
-        enc_data.setup()
-
-        # Get the model, experiment, logger set up
-        config['encoder']['init_size'] = enc_data.train_dataset.data_len
-        encoder = Encoder(**config['encoder'])
-        logger = loggers.TensorBoardLogger(config['encoder']['training']['log_dir'], version=0, name=config['encoder']['name'])
-        expected_lr = max((config['encoder']['lr'] * config['encoder']['scheduler_gamma'] ** (config['encoder']['training']['max_epochs'] *
-                                                                                          config['encoder']['training']['swa_start'])),
-                          1e-9)
-        enc_trainer = Trainer(logger=logger, max_epochs=config['encoder']['training']['max_epochs'],
-                          default_root_dir=config['encoder']['training']['weights_path'],
-                          log_every_n_steps=config['encoder']['training']['log_epoch'], callbacks=
-                          [EarlyStopping(monitor='train_loss', patience=config['encoder']['training']['patience'],
-                                         check_finite=True),
-                           StochasticWeightAveraging(swa_lrs=expected_lr,
-                                                     swa_epoch_start=config['encoder']['training']['swa_start']),
-                           ModelCheckpoint(monitor='train_loss')])
-
-        print("======= Encoder Training =======")
-        try:
-            if config['encoder']['training']['warm_start']:
-                enc_trainer.fit(encoder, ckpt_path=f"{config['encoder']['training']['weights_path']}/{config['encoder']['name']}.ckpt",
-                            datamodule=enc_data)
-            else:
-                enc_trainer.fit(encoder, datamodule=enc_data)
-        except KeyboardInterrupt:
-            if enc_trainer.is_global_zero:
-                print('Training interrupted.')
-            else:
-                print('adios!')
-                exit(0)
-        if config['encoder']['training']['save_model']:
-            enc_trainer.save_checkpoint(f"{config['encoder']['training']['weights_path']}/{config['encoder']['name']}.ckpt")
-
-        # Run this through all the data and get averages
-        encoder.eval()
-        av_data = pd.read_csv(f'{config["dataloader"]["datapath"]}/MAverages.csv').set_index(['season', 'tid'])
-        enc_df = pd.DataFrame(index=av_data.index, columns=np.arange(encoder.latent_size), dtype=np.float32)
-        for chunk in range(0, av_data.shape[0], 32):
-            tmp = av_data.iloc[chunk:chunk+32]
-            ed = encoder.encode(torch.tensor(tmp.values, dtype=torch.float32))
-            enc_df.loc[tmp.index] = ed.detach().numpy()
-
-        if config['encoder']['training']['save_model']:
-            enc_df.to_csv(f'{config["dataloader"]["datapath"]}/MEncodedData.csv')
-    else:
-        print('Skipping encoder training.')
-        enc_df = pd.read_csv(f'{config["dataloader"]["datapath"]}/MEncodedData.csv').set_index(['season', 'tid'])
+    enc_df = pd.read_csv(f'{config["dataloader"]["datapath"]}/MEncodedData.csv').set_index(['season', 'tid'])
 
     data = PredictorDataModule(**config['dataloader'], file=enc_df)
     data.setup()